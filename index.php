3d5cf3954860c7de82e1a9caa9d2bfde992677c8
www-data <www-data@daniel-desktop.(none)>
2011-07-15 11:41:04 +0200

    automatic commit 15-07-2011 11:07:04

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo 'dit is de index';
+echo '<br />';
+echo 'pagina 1: de betekenis van het leven';
+echo '<br />';
+echo 'pagina 2: de kunst van het broodje kaas';
+echo '<br />';
+echo 'pagina 3: staart-recursieve argumenten voor veel voorkomende ideologieen';
+echo '<br />';
+echo 'pagina 4: kop-recursieve argumenten voor kortstondige doch levensgevaarlijke radicale ideologieen';
+echo '<br />';
+echo 'pagina 5: het paradoxaal irrationeel rationalisme';
